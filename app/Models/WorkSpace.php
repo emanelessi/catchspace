743e16253d8972bb67648ef5f3739f9f43ca0090
emanelessi <hanieman86@gmail.com>
2022-02-18 00:04:46 +0200

    commit

--- app/Models/WorkSpace.php
+++ app/Models/WorkSpace.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class WorkSpace extends Model
 {
     use HasFactory;
+    public function workSpaceType()
+    {
+        return $this->belongsTo(WorkSpaceType::class, 'work_space_type_id');
+    }
+    public function provider()
+    {
+        return $this->belongsTo(Provider::class, 'provider_id');
+    }
 }
