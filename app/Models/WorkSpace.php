514b18d1f485d03b688605d424a3cee69753175a
DinaAkilaDev <dinashadiakeela@gmail.com>
2022-02-18 00:21:25 +0200

    commit

--- app/Models/WorkSpace.php
+++ app/Models/WorkSpace.php
@@ -16,4 +16,16 @@ class WorkSpace extends Model
     {
         return $this->belongsTo(Provider::class, 'provider_id');
     }
+    public function workers(){
+        return $this->hasMany(Worker::class,'work_space_id','id');
+    }
+    public function workSpaceAddons(){
+        return $this->hasMany(WorkSpaceAddons::class,'work_space_id','id');
+    }
+    public function pricing(){
+        return $this->hasMany(Pricing::class,'work_space_id','id');
+    }
+    public function workSpaceServices(){
+        return $this->hasMany(WorkSpaceService::class,'work_space_id','id');
+    }
 }
