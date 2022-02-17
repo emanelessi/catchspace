e8d44b16a568ac92ca63ae3b14d200f381fdddb8
emanelessi <hanieman86@gmail.com>
2022-02-17 23:33:27 +0200

    commit

--- app/Models/WorkSpaceType.php
+++ app/Models/WorkSpaceType.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class WorkSpaseType extends Model
+class WorkSpaceType extends Model
 {
     use HasFactory;
 }
