5270323625fd05356f6df8f3bb9506426c6c394c
DinaAkilaDev <dinashadiakeela@gmail.com>
2022-03-01 10:13:23 +0200

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         $this->call(PermissionSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(ProviderTableSeeder::class);
+        $this->call(RoleSeeder::class);
 
     }
 }
