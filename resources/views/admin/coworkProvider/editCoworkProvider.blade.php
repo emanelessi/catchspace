24eb5fcb4df8af7e4ae78b2188b39e0067b69828
DinaAkilaDev <dinashadiakeela@gmail.com>
2022-03-07 13:13:30 +0200

    commit

--- resources/views/admin/coworkProvider/editCoworkProvider.blade.php
+++ resources/views/admin/coworkProvider/editCoworkProvider.blade.php
@@ -6,6 +6,41 @@
             background-color: #8B62C9 !important;
         }
 
+        .demo-loading-btn:hover {
+            background-color: #D2CDCD54 !important;
+        }
+
+        .inputfile {
+            width: 0.1px;
+            height: 0.1px;
+            opacity: 0;
+            overflow: hidden;
+            position: absolute;
+            z-index: -1;
+        }
+
+        .inputfile + label {
+            border-color: #eaeaea;
+            margin-top: 60px;
+
+            align-items: center;
+            padding: 5px;
+            border-radius: 8px !important;
+            background-color: #eaeaea;
+            font-family: AvenirLTStd-Book;
+            font-size: 14px;
+            line-height: 24px;
+            color: #6B7280;
+            width: 157px;
+            display: inline-block;
+        }
+
+        .inputfile:focus + label,
+        .inputfile + label:hover {
+            background-color: #7952b3;
+            color: white;
+
+        }
     </style>
     <div class="page-head" style="margin-bottom: 25px;">
         <!-- BEGIN PAGE TITLE -->
@@ -15,113 +50,112 @@
             font-size: 30px;
             letter-spacing: -0.022em;
             color: #333333;
-            " class="margin-top--10 ">Cowork provider</h1>
+            " class="margin-top--10 ">Edit Cowork provider</h1>
         </div>
         <!-- END PAGE TITLE -->
-        <div class="row ">
-            <div class="col-md-12">
-                <!-- BEGIN PROFILE SIDEBAR -->
-                <div class="m-grid-col-lg-12 m-grid-col-md-12 m-grid-col-xs-12 ">
-                    <!-- PORTLET MAIN -->
-                    <div class=" portlet light profile-sidebar-portlet bordered margin-top-35"
-                         style="border-bottom: 1px solid #eef1f5;height: 100%;border-radius: 20px !important;">
-                        <!-- SIDEBAR USERPIC -->
-                        <div class=" profile-userpic margin-right--12 margin-top-25" style="
+            <div class="row">
+                <div class="col-md-12 col-lg-12 col-xs-12">
+
+                    <!-- PERSONAL INFO TAB -->
+                    <div class="tab-pane active" id="tab_1_1">
+                        <form role="form" action="{{route('providerupdate')}}" enctype="multipart/form-data"
+                              method="Post">
+                            @csrf
+
+                            <div class="profile-userpic  margin-right--12 "
+                                 style="
                         float: left;
                         width: 300px;
-                        /*margin-right: -35px;*/
 ">
-                            <img src="../assets/pages/media/profile/profile_user.jpg" class="img-responsive" alt="">
-                        </div>
-                        <!-- END SIDEBAR USERPIC -->
-                        <!-- SIDEBAR USER TITLE -->
-                        <div class=" profile-usertitle margin-top-70" style="
+                                <img src="/storage/{{$provider->logo}}"
+                                     class="img-responsive" alt="">
+                            </div>
+                            <!-- END SIDEBAR USERPIC -->
+                            <!-- SIDEBAR USER TITLE -->
+                            <b class="profile-usertitle margin-top-40" style="
                         text-align: justify;
                         /*margin-top: 47px;*/
 ">
-                            <div class="profile-usertitle-name" style="
-/*font-style: normal;*/
-/*font-weight: 600;*/
-font-size: 24px;
-line-height: 137.5%;
-color: #111827;
-             font-family: AvenirLTStd-Book;
-"> Omar D.
-                            </div>
-                            <div style="
-/*font-style: normal;*/
-/*font-weight: normal;*/
-font-size: 14px;
-line-height: 157%;
-color: #6B7280;
-             font-family: AvenirLTStd-Book;
-"> Los Angeles USA
-                            </div>
-                        </div>
-                        <!-- END SIDEBAR USER TITLE -->  <!-- BEGIN PROFILE CONTENT -->
-                        <div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="portlet light bordered" style="
-                                        margin: 72px 73px 200px;
+                                <input type="file" data-loading-text="Loading..."
+                                       name="logo" id="file"
+                                       class="inputfile demo-loading-btn btn btn-primary margin-top-85"
+                                       value="{{$provider->logo}}"
+                                       style="
+
+                                         border-color:#eaeaea;
+
+                                         align-items: center;
+                                         padding: 5px;
+                                         border-radius: 8px !important;
+                                         background-color: #eaeaea;
+                                         font-family: AvenirLTStd-Book;
+                                         font-size: 14px;
+                                         line-height: 24px;
+                                         color: #6B7280;
+                                         width: 157px;
+                                         "
+                                >
+
+
+                                <label for="file"> Upload wokspace logo <i class="fa fa-file-photo-o"
+                                                                           style="color: #6B7280 !important;"></i></label>
+
+                                <div class="portlet light bordered" style="
+                                        margin: 110px 73px 200px;
                                         border-radius: 20px !important;
                                         background-color: #fff;
                                          border-top-width: 2px !important;
                                             border-bottom-width: 2px !important;
 ">
-                                        <div class="portlet-body">
-                                            <div class="tab-content">
-                                                <!-- PERSONAL INFO TAB -->
-                                                <div class="tab-pane active" id="tab_1_1">
-                                                    <form role="form" action="#">
-                                                        <div class="form-group col-md-6">
-                                                            <label class="control-label margin-left-9"
-                                                                   style="color:#6B7280;">Workspace
-                                                                Name *</label>
-                                                            <input type="text" placeholder="Tools" class="form-control"
-                                                                   style="
+                                    <div class="portlet-body">
+                                        <div class="tab-content">
+                                            <div class="form-group margin-left-9 margin-right-10"
+                                                 style="
+                                                             /*margin-left: 16px;*/
+                                                             /*margin-right: 16px;*/">
+                                                <label class="control-label margin-left-9"
+                                                       style="color:#6B7280;">
+                                                    Name*</label>
+                                                <input type="text" name="name"
+                                                       placeholder="Tools" value="{{$provider->name}}"
+                                                       class="form-control" style="
                                                                        height: 40px;
-                                                                       /*padding: 0 12px;*/
-                                                                       border-radius: 8px !important;
-                                                                       border: solid 1px #d1d5db;"/>
-                                                        </div>
-                                                        <div class="form-group col-md-6">
-                                                            <label class="control-label margin-left-9"
-                                                                   style="color:#6B7280;">Owner
-                                                                Name *</label>
-                                                            <input type="text" placeholder="Omar D."
-                                                                   class="form-control"
-                                                                   style="
-                                                                       height: 40px;
-                                                                       /*padding: 0 12px;*/
-                                                                       border-radius: 8px !important;
-                                                                       border: solid 1px #d1d5db;"/>
-                                                        </div>
-                                                        <div class="form-group margin-left-9 margin-right-10"
-                                                             style="
+                                                                       /*height: 56px;padding: 0 12px;*/
+                                                                       border-radius: 8px !important;border: solid 1px #d1d5db;"/>
+                                            </div>
+                                            <div class="form-group margin-left-9 margin-right-10"
+                                                 style="
                                                              /*margin-left: 16px;*/
                                                              /*margin-right: 16px;*/
 ">
-                                                            <label class="control-label margin-left-9"
-                                                                   style="color:#6B7280;">Workspace
-                                                                Address*</label>
-                                                            <input type="text"
-                                                                   placeholder="Al-rehab mall, remal street, Gaza, palestine"
-                                                                   class="form-control" style="
+                                                <label class="control-label margin-left-9"
+                                                       style="color:#6B7280;">
+                                                    Owner Name*</label>
+                                                <input type="text" name="owner_name"
+                                                       placeholder="Rami" value="{{$provider->owner_name}}"
+                                                       class="form-control" style="
                                                                        height: 40px;
-                                                                       /*padding: 0 12px;*/
-                                                                       border-radius: 8px !important;
-                                                                       border: solid 1px #d1d5db;"/></div>
-                                                    </form>
-                                                </div>
-
-                                                <!-- END PERSONAL INFO TAB -->
+                                                                       /*height: 56px;padding: 0 12px;*/
+                                                                       border-radius: 8px !important;border: solid 1px #d1d5db;"/>
                                             </div>
-                                        </div>
-                                    </div>
-                                    <button type="button"
-                                            class="btn btn-primary margin-top--250  m-grid-col-lg-offset-9-5 m-grid-col-md-offset-9-5 m-grid-col-xs-offset-7 "
-                                            style="
+                                            <div class="form-group margin-left-9 margin-right-10"
+                                                 style="
+                                                             /*margin-left: 16px;*/
+                                                             /*margin-right: 16px;*/
+">
+                                                <label class="control-label margin-left-9"
+                                                       style="color:#6B7280;">
+                                                    Address*</label>
+                                                <input type="text" name="address" value="{{$provider->address}}"
+                                                       placeholder="Al-rehab mall, remal street, Gaza, palestine"
+                                                       class="form-control" style="
+                                                                       height: 40px;
+                                                                       /*height: 56px;padding: 0 12px;*/
+                                                                       border-radius: 8px !important;border: solid 1px #d1d5db;"/>
+                                            </div>
+                                            <button type="submit"
+                                                    class="btn btn-primary margin-top-25  m-grid-col-lg-offset-9-5 m-grid-col-md-offset-9-5 m-grid-col-xs-offset-7 "
+                                                    style="
                                                                width: 125px;
                                                                height: 32.2px;
                                                                /*flex-grow: 0;*/
@@ -143,18 +177,12 @@ color: #6B7280;
                                                                font-size: 13px;
                                                                letter-spacing: 1px;
                                                                ">
-                                        Save Change
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- END PROFILE CONTENT -->
+                                                Save Change
+                                            </button>
+                        </form>
                     </div>
-                    <!-- END PORTLET MAIN -->
-
+                    <!-- END PERSONAL INFO TAB -->
                 </div>
-                <!-- END BEGIN PROFILE SIDEBAR -->
-
             </div>
         </div>
 
