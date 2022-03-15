83f5d39085aa941f19b159e1972b6a9739a67f15
emanelessi <hanieman86@gmail.com>
2022-03-15 13:49:58 +0200

    commit

--- resources/views/admin/providerProfile/editProfile.blade.php
+++ resources/views/admin/providerProfile/editProfile.blade.php
@@ -1,10 +1,12 @@
 @extends('admin.layouts.index')
 
 @section('content')
+    <!-- BEGIN PAGE style -->
     <style>
         .btn:hover {
             background-color: #8B62C9 !important;
         }
+
         .page-head {
             margin-bottom: 25px !important;
         }
@@ -14,121 +16,6 @@
             font-size: 33px !important;
             letter-spacing: -0.022em !important;
             color: #333333 !important;
-        }
-
-        .add-btn {
-            border-color: #7952B3 !important;
-            font-family: AvenirLTStd-Book !important;
-            color: #FFFFFF !important;
-            background: #7952B3 !important;
-            border-radius: 5px !important;
-            width: 160px !important;
-            height: 30px !important;
-            font-size: 13px !important;
-        }
-
-        .btn-filter {
-            background-color: #FFFFFF !important;
-            border-color: #FFFFFF !important;
-            border-radius: 5px !important;
-            font-family: AvenirLTStd-Book !important;
-            font-weight: normal;
-        !important font-size: 13 px !important;
-            color: #292D32 !important;
-            width: 90px !important;
-            height: 29px !important;
-            margin-right: -20px !important;
-        }
-
-        .body-filter {
-            background: #FFFFFF !important;
-            border: 1px solid #F4F4F4 !important;
-            box-sizing: border-box !important;
-            border-radius: 10px !important;
-        }
-
-        .span-filter {
-            color: #fff !important;
-            background: #7952B3 !important;
-            border-radius: 5px !important;
-            font-family: AvenirLTStd-Book !important;
-            font-weight: normal !important;
-            font-size: 15px !important;
-            line-height: 15px !important;
-        }
-
-        .table-th {
-            font-family: AvenirLTStd-Book !important;
-            font-size: 20px !important;
-            letter-spacing: -0.022em !important;
-            color: #e7ae04 !important;
-            padding-bottom: 15px !important;
-        }
-
-        .table-td {
-            font-family: AvenirLTStd-Book !important;
-            font-size: 20px !important;
-            letter-spacing: 0.02em !important;
-            color: #000000 !important;
-        }
-
-        .modal-back {
-            background: rgb(255, 255, 255) !important;
-            border-radius: 10px !important;
-            display: block !important;
-            margin-top: -82.5px !important;
-            height: 185px !important;
-            width: 480px !important;
-        }
-
-        .modal-p {
-            font-family: AvenirLTStd-Book !important;
-            font-weight: bold !important;
-            font-size: 18px !important;
-            line-height: 24px !important;
-            text-align: center !important;
-            letter-spacing: -0.011em !important;
-            color: #333333 !important;
-        }
-
-        .btn-cansel {
-            width: 125px !important;
-            height: 32.2px !important;
-            align-items: center !important;
-            padding: 0 !important;
-            border-radius: 8px !important;
-            font-family: AvenirLTStd-Book !important;
-            font-size: 13px !important;
-            letter-spacing: 1px !important;
-            border-radius: 10px !important;
-            border-color: #7952B3 !important;
-        }
-
-        .btn-delete {
-            width: 125px !important;
-            height: 32.2px !important;
-            border-color: #7952b3 !important;
-            align-items: center !important;
-            padding: 0 !important;
-            border-radius: 8px !important;
-            background-color: #7952b3 !important;
-            font-family: AvenirLTStd-Book !important;
-            color: #FFFFFF !important;
-            font-size: 13px !important;
-            letter-spacing: 1px !important;
-        }
-
-        .img-logo {
-            border-color: #eaeaea !important;
-            align-items: center !important;
-            padding: 5px !important;
-            border-radius: 8px !important;
-            background-color: #eaeaea !important;
-            font-family: AvenirLTStd-Book !important;
-            font-size: 14px !important;
-            line-height: 24px !important;
-            color: #6B7280 !important;
-            width: 157px !important;
         }
 
         .form-input {
@@ -151,15 +38,12 @@
             letter-spacing: 1px !important;
         }
     </style>
-    <div class="page-head" style="margin-bottom: 25px;">
+    <!-- END PAGE style -->
+
+    <div class="page-head">
         <!-- BEGIN PAGE TITLE -->
         <div class="page-title">
-            <h1 style="
-            font-family: AvenirLTStd-Book;
-            font-size: 30px;
-            letter-spacing: -0.022em;
-            color: #333333;
-            " class="margin-top--10 ">Cowork provider</h1>
+            <h1 class="margin-top--10 head-title">Cowork provider</h1>
         </div>
         <!-- END PAGE TITLE -->
         <div class="row">
@@ -173,7 +57,6 @@
                         <div class=" profile-userpic margin-right--12 margin-top-25" style="
                         float: left;
                         width: 300px;
-                        /*margin-right: -35px;*/
 ">
                             <img src="../assets/pages/media/profile/profile_user.jpg" class="img-responsive" alt="">
                         </div>
@@ -181,11 +64,8 @@
                         <!-- SIDEBAR USER TITLE -->
                         <div class=" profile-usertitle margin-top-70" style="
                         text-align: justify;
-                        /*margin-top: 47px;*/
 ">
                             <div class="profile-usertitle-name" style="
-/*font-style: normal;*/
-/*font-weight: 600;*/
 font-size: 24px;
 line-height: 137.5%;
 color: #111827;
@@ -194,8 +74,6 @@ color: #111827;
                                 Ali Mahdi
                             </div>
                             <div style="
-/*font-style: normal;*/
-/*font-weight: normal;*/
 font-size: 14px;
 line-height: 157%;
 color: #6B7280;
@@ -226,24 +104,14 @@ color: #6B7280;
                                                                    style="color:#6B7280;">Name
                                                                 *</label>
                                                             <input type="text" placeholder="Ali Mahdi"
-                                                                   class="form-control"
-                                                                   style="
-                                                                       height: 40px;
-                                                                       /*padding: 0 12px;*/
-                                                                       border-radius: 8px !important;
-                                                                       border: solid 1px #d1d5db;"/>
+                                                                   class="form-control form-input"/>
                                                         </div>
                                                         <div class="form-group col-md-6">
                                                             <label class="control-label margin-left-9"
                                                                    style="color:#6B7280;">Job
                                                                 Title *</label>
                                                             <input type="text" placeholder="Programmer "
-                                                                   class="form-control"
-                                                                   style="
-                                                                       height: 40px;
-                                                                       /*padding: 0 12px;*/
-                                                                       border-radius: 8px !important;
-                                                                       border: solid 1px #d1d5db;"/>
+                                                                   class="form-control form-input"/>
                                                         </div>
                                                         <div class="form-group "
                                                              style="margin-left: 16px;margin-right: 16px;">
@@ -252,12 +120,7 @@ color: #6B7280;
                                                                 *</label>
                                                             <input type="text"
                                                                    placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing  "
-                                                                   class="form-control"
-                                                                   style="
-                                                                       height: 40px;
-                                                                       /*padding: 0 12px;*/
-                                                                       border-radius: 8px !important;
-                                                                       border: solid 1px #d1d5db;"/>
+                                                                   class="form-control form-input"/>
                                                         </div>
                                                         <div class="form-group "
                                                              style="margin-left: 16px;margin-right: 16px;">
@@ -266,12 +129,7 @@ color: #6B7280;
                                                                 *</label>
                                                             <input type="text"
                                                                    placeholder="Active"
-                                                                   class="form-control"
-                                                                   style="
-                                                                       height: 40px;
-                                                                       /*padding: 0 12px;*/
-                                                                       border-radius: 8px !important;
-                                                                       border: solid 1px #d1d5db;"/>
+                                                                   class="form-control form-input"/>
                                                         </div>
                                                     </form>
                                                 </div>
@@ -281,29 +139,7 @@ color: #6B7280;
                                         </div>
                                     </div>
                                     <button type="button"
-                                            class="btn btn-primary margin-top--250  m-grid-col-lg-offset-9-5 m-grid-col-md-offset-9-5 m-grid-col-xs-offset-7 "
-                                            style="
-                                                               width: 125px;
-                                                               height: 32.2px;
-                                                               /*flex-grow: 0;*/
-                                                               /*display: flex;*/
-                                                               /*flex-direction: column;*/
-                                                               /*justify-content: center;*/
-                                                               border-color: #7952b3;
-                                                               align-items: center;
-                                                               /*margin: -155px 0 0 755px;*/
-                                                               padding: 0;
-                                                               border-radius: 8px !important;
-                                                               background-color: #7952b3;
-                                                               font-family: AvenirLTStd-Book;
-                                                               /*font-style: normal;*/
-                                                               /*font-weight: 600;*/
-                                                               /*font-size: 14px;*/
-                                                               /* line-height: 24px; */
-                                                               color: #FFFFFF;
-                                                               font-size: 13px;
-                                                               letter-spacing: 1px;
-                                                               ">
+                                            class="btn btn-submit btn-primary margin-top--250  m-grid-col-lg-offset-9-5 m-grid-col-md-offset-9-5 m-grid-col-xs-offset-7 ">
                                         Save Change
                                     </button>
                                 </div>
